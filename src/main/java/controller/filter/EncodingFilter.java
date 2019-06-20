398c57f757494651193b89d59249eddc9215059a
IlliaKorchan <ko.illia97@gmail.com>
2019-06-20 10:20:19 +0300

    V0.9.3 Access closed to logout users

--- src/main/java/controller/filter/EncodingFilter.java
+++ src/main/java/controller/filter/EncodingFilter.java
@@ -1,8 +1,6 @@
 package controller.filter;
 
 import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
